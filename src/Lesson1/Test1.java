aa5469178ec09cc4bf4e8de1eb3b7b4cd1b7f4a5
Elnar <kuryaevelnar@gmail.com>
2022-08-25 12:31:02 +0300

    The first lesson

---
+++ src/Lesson1/Test1.java
@@ -0,0 +1,12 @@
+package Lesson1;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("abcdef");
+
+        System.out.println();
+
+        System.out.print("ghijkl");
+    }
+}
